8b61f0a1b650e206812cec4801c8561f8e8709ca
Jamal raouj <jamalrwj@gmail.com>
2022-11-02 16:33:06 +0100

    Added login servlet

--- src/main/java/Service/Implimentation/ServiceUserImp.java
+++ src/main/java/Service/Implimentation/ServiceUserImp.java
@@ -1,11 +1,16 @@
 package Service.Implimentation;
 
+import Dao.Implementation.DaoUserImp;
+import Dao.Interface.DaoUser;
 import Entity.User;
 import Service.Interface.UserService;
 
 public class UserServiceImp implements UserService {
+    DaoUser daoUserImp = new DaoUserImp();
+
     @Override
     public User doLogin(String email, String password) {
+
         return null;
     }
 }
