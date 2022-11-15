edbaad6c62a419ab6180d41b89520b92e36b2d24
saidaBRKT <saidabarakat06@gmail.com>
2022-11-15 09:16:56 +0100

    Résoudre un conflit

--- src/main/java/Service/Implimentation/ServiceResponsableImp.java
+++ src/main/java/Service/Implimentation/ServiceResponsableImp.java
@@ -2,21 +2,18 @@ package Service.Implimentation;
 
 import Dao.Implementation.DaoResponsableImp;
 import Dao.Interface.IDao;
-<<<<<<< HEAD
-=======
-import Entity.Person;
->>>>>>> main
+
+
 import Entity.Responsable;
 import Service.Interface.ServiceInterface;
 
 import java.util.List;
 
 public class ServiceResponsableImp implements ServiceInterface<Responsable> {
-<<<<<<< HEAD
-=======
+
     IDao daoResponsableImp = new DaoResponsableImp();
 
->>>>>>> main
+
     @Override
     public void insert(Responsable o) {
 
@@ -29,25 +26,23 @@ public class ServiceResponsableImp implements ServiceInterface<Responsable> {
 
     @Override
     public List<Responsable> findAll() {
-<<<<<<< HEAD
+
         IDao daoResponsableImp = new DaoResponsableImp();
         return daoResponsableImp.findAll();
-=======
-        return daoResponsableImp.findAll();
 
->>>>>>> main
+
+
     }
 
     @Override
     public void update(Responsable o) {
 
     }
-<<<<<<< HEAD
-=======
+
 
     @Override
     public Responsable findById(long t) {
         return (Responsable) daoResponsableImp.findById(t);
     }
->>>>>>> main
+
 }
